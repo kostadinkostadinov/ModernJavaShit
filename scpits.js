057c2627b6a23558522e7c2e004c86e1a9cfe57a
Kostadin Kostadinov <kostadinovkostadin6@gmail.com>
2013-01-24 11:41:42 +0200

    Revert "Revert "forms ready""
    This reverts commit 75bc8f0d3e1d17acee14001a13919b516835191f.

---
+++ scpits.js
@@ -0,0 +1,12 @@
+function OnBtnLoginClick() 
+{
+	var userInfo = {
+		Username: $('#Textbox_Username').val();
+		Password: $('#Textbox_password').val();
+	};
+	
+	$.ajax ({
+		url: "url",
+		
+	});
+}
